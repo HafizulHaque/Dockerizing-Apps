07d3ad84edefb6b07538e05cd623bca4c356e160
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2024-01-14 19:40:54 +0600

    initial commit

---
+++ react-docker/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="main">
+      <h1>React is Awesome!</h1>
+      <p>Using Docker is awesome</p>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Hello Docker</h1>
+      <p>Testing dockerizing react app</p>
+    </div>
+  )
+}
+
+export default App
